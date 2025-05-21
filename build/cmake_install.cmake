6359237dc513625838b4a91e51f88e77a1f8ee35
Eugen <72526004+shagunov@users.noreply.github.com>
2025-05-21 23:37:25 +0300

    кеке

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/Project")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files/VulkanSamples")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
